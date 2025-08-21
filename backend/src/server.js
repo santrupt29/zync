aba52e067c385f51aaf12a65769ba2d4e7f1e0b6
Santrupt Potphode <santrupt.potphode29@gmail.com>
2025-08-21 14:36:29 +0530

    DB initialised and ENV setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const PORT = ENV.PORT || 3000;
 
 app.listen(PORT || 3000, () => {
   console.log("Server is running on port 3000");
+  connectDB();
 });
 
 app.get("/", (req, res) => {
   res.send("Hello World 123");
 });
+
