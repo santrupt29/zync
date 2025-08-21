6ddd2c12ecf3d1ca109017e3b3538b3574f0b975
Santrupt Potphode <santrupt.potphode29@gmail.com>
2025-08-21 19:26:12 +0530

    Minor changes

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
+import { User } from "../models/user.model.js";
 
 export const inngest = new Inngest({
   id: "zync",
@@ -23,6 +24,7 @@ const deleteUserFromDB = inngest.createFunction(
     {id: "delete-user-from-db"},
     {event: "clerk/user.deleted"},
     async({event}) => {
+        await connectDB()
         const {id} = event.data;
         await User.deleteOne({clerkId:id})
     }
