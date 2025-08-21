4615b88e10040234debad73832eaaa38727bbf12
Santrupt Potphode <santrupt.potphode29@gmail.com>
2025-08-21 21:58:26 +0530

    Fixed inngest/js

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -6,19 +6,44 @@ export const inngest = new Inngest({
   id: "zync",
 });
 
+// const syncUser = inngest.createFunction(
+//     {id: "sync-user"},
+//     {event: "clerk/user.created"},
+//     async({event}) => {
+//         await connectDB();
+//         const {id, email_addresses, first_name, last_name} = event.data.object;
+//         const user = await User.findOne({clerkId: id});
+//         if(!user){
+//             const newUser = new User({clerkId: id, email: email_addresses[0], name:`${first_name || ""} ${last_name || ""}`, image : image_url});
+//             await newUser.save();
+//         }
+//     }
+// );
 const syncUser = inngest.createFunction(
     {id: "sync-user"},
     {event: "clerk/user.created"},
     async({event}) => {
         await connectDB();
-        const {id, email_addresses, first_name, last_name} = event.data.object;
+        const {id, email_addresses, first_name, last_name, image_url} = event.data;
+        
+        if (!email_addresses || email_addresses.length === 0) {
+            console.error('No email addresses found for user:', id);
+            return;
+        }
+        
         const user = await User.findOne({clerkId: id});
         if(!user){
-            const newUser = new User({clerkId: id, email: email_addresses[0], name:`${first_name || ""} ${last_name || ""}`, image : image_url});
+            const newUser = new User({
+                clerkId: id, 
+                email: email_addresses[0].email_address, 
+                name: `${first_name || ""} ${last_name || ""}`.trim(), 
+                image: image_url || ""
+            });
             await newUser.save();
         }
     }
 );
+
 
 const deleteUserFromDB = inngest.createFunction(
     {id: "delete-user-from-db"},
