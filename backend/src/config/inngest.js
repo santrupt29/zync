3082684ca851e03462eaf26f2f473a085e91ef22
Santrupt Potphode <santrupt.potphode29@gmail.com>
2025-08-21 22:06:29 +0530

    Minor Changes

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -20,29 +20,31 @@ export const inngest = new Inngest({
 //     }
 // );
 const syncUser = inngest.createFunction(
-    {id: "sync-user"},
-    {event: "clerk/user.created"},
-    async({event}) => {
-        await connectDB();
-        const {id, email_addresses, first_name, last_name, image_url} = event.data;
-        
-        if (!email_addresses || email_addresses.length === 0) {
-            console.error('No email addresses found for user:', id);
-            return;
-        }
-        
-        const user = await User.findOne({clerkId: id});
-        if(!user){
-            const newUser = new User({
-                clerkId: id, 
-                email: email_addresses[0].email_address, 
-                name: `${first_name || ""} ${last_name || ""}`.trim(), 
-                image: image_url || ""
-            });
-            await newUser.save();
-        }
+    { id: "sync-user" },
+    { event: "clerk/user.created" },
+    async ({ event }) => {
+      await connectDB();
+  
+      const { id, email_addresses, first_name, last_name, image_url } = event.data;
+  
+      const newUser = {
+        clerkId: id,
+        email: email_addresses[0]?.email_address,
+        name: `${first_name || ""} ${last_name || ""}`,
+        image: image_url,
+      };
+  
+      await User.create(newUser);
+  
+      await upsertStreamUser({
+        id: newUser.clerkId.toString(),
+        name: newUser.name,
+        image: newUser.image,
+      });
+  
+      await addUserToPublicChannels(newUser.clerkId.toString());
     }
-);
+  );
 
 
 const deleteUserFromDB = inngest.createFunction(
