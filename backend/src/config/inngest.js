884cd8460ee83ae18566c34e42f81cd1595703f6
Santrupt Potphode <santrupt.potphode29@gmail.com>
2025-08-25 12:10:42 +0530

    Fixed errors

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -2,6 +2,7 @@ import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js";
 import { deleteStreamUser } from "./stream.js";
+import { upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({
   id: "zync",
