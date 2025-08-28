5ce6c2c408c695543e5cdcdf1fc83dd86e6033e0
Santrupt Potphode <santrupt.potphode29@gmail.com>
2025-08-28 13:13:10 +0530

    Deployment  ready

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,9 @@
 
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://zync-backend.vercel.app/api";
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://zync-backend.vercel.app/api";
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
