5115e8d929d81099c97141b366ad1f9428d60659
Santrupt Potphode <santrupt.potphode29@gmail.com>
2025-09-09 14:38:30 +0530

    Minor error fixed

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -4,7 +4,7 @@ export const getStreamToken = async (req, res) => {
   try {
     const token = await generateStreamToken(req.auth().userId);
     res.status(200).json({token});
-    res.send(token);
+    // res.send(token);
   } catch (error) {
     console.log("Error generating stream token:", error);
     res.status(500).json({message: "Failed to generate stream token"});
